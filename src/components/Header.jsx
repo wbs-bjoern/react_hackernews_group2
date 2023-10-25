57e85189559e4525f24b99f8d822972502ea2de6
unknown <you@example.com>
2023-10-25 18:20:37 +0200

    add pages + header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+
+
+
+export default function Header () {
+
+return (
+<nav>
+    <ul>
+        <li>Logo</li>
+        <li>Titel</li>
+        <li>new</li>
+        <li>past</li>
+        <li>comments</li>
+        <li>show</li>
+        <li>jobs</li>
+        <li>submit</li>
+        <li>login</li>
+    </ul>
+
+</nav>
+);
+};
